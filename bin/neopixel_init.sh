84031a52ae338d592698f939ca19246497e7da99
Nick Horvath <nhorvath@gmail.com>
2017-10-16 22:42:22 -0400

    logfiles

--- bin/neopixel_init.sh
+++ bin/neopixel_init.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+LOGFILE=/var/log/neopixel.log
 
 if [ $# -eq 1 ] ; then
   DEV=$1
@@ -10,7 +11,7 @@ stty -F $DEV cs8 9600 ignbrk -brkint -icrnl -imaxbel -opost -onlcr -isig -icanon
 
 TAIL_PROC=`ps aux | grep -v grep | grep "tail -f $DEV"`
 if [ $? -eq 1 ] ; then
-  tail -f $DEV &
+  tail -f $DEV >> $LOGFILE &
   sleep 2
 fi
 
